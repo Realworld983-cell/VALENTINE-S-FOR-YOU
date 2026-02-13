d51c4f33288e405a5efa885ea256724fc993b59b
Realworld983-cell <tusharlenka141@gmail.com>
2026-02-13 22:27:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 const noBtn = document.getElementById("no");
 
+// Make sure button can move
+noBtn.style.position = "absolute";
+
 noBtn.addEventListener("mouseover", () => {
   noBtn.style.top = Math.random() * 80 + "vh";
   noBtn.style.left = Math.random() * 80 + "vw";
 });
 
-document.getElementById("yes").onclick = function() {
+document.getElementById("yes").onclick = function () {
   document.getElementById("prankPage").style.display = "none";
   document.getElementById("slideshow").style.display = "flex";
-  document.getElementById("bgMusic").play();
+
+  const music = document.getElementById("bgMusic");
+  music.play().catch(() => {
+    console.log("Autoplay blocked until user interaction");
+  });
+
   startSlideshow();
 };
 
 const images = [
-  {src:"IMG-20260210-WA0008.jpg",text:"The day you came into my life ❤️"},
-  {src:"IMG-20260210-WA0005.jpg",text:"Your smile is my happiness 😊"},
-  {src:"IMG-20260210-WA0006.jpg",text:"Every moment with you is magical ✨"},
-  {src:"IMG-20260210-WA0007.jpg",text:"You are my safe place 💕"},
-  {src:"IMG-20260210-WA0008.jpg",text:"Forever starts with you 💖"},
-  {src:"IMG-20260210-WA0004.jpg",text:"My heart beats for you 💓"},
-  {src:"IMG-20260210-WA0006.jpg",text:"Will You My Velentine❤️"}
+  { src: "IMG-20260210-WA0008.jpg", text: "The day you came into my life ❤️" },
+  { src: "IMG-20260210-WA0005.jpg", text: "Your smile is my happiness 😊" },
+  { src: "IMG-20260210-WA0006.jpg", text: "Every moment with you is magical ✨" },
+  { src: "IMG-20260210-WA0007.jpg", text: "You are my safe place 💕" },
+  { src: "IMG-20260210-WA0008.jpg", text: "Forever starts with you 💖" },
+  { src: "IMG-20260210-WA0004.jpg", text: "My heart beats for you 💓" },
+  { src: "IMG-20260210-WA0006.jpg", text: "Will You Be My Valentine ❤️" }
 ];
 
 let index = 0;
@@ -27,9 +35,14 @@ const slide = document.getElementById("slide");
 const caption = document.getElementById("caption");
 const background = document.querySelector(".background");
 
-function startSlideshow(){
+function startSlideshow() {
+
+  // 🔥 VERY IMPORTANT: Set first image
+  slide.src = images[0].src;
   background.style.backgroundImage = `url(${images[0].src})`;
   caption.innerText = images[0].text;
+
+  slide.style.opacity = 1;
   caption.style.opacity = 1;
 
   setInterval(() => {
@@ -38,7 +51,7 @@ function startSlideshow(){
 
     setTimeout(() => {
       index++;
-      if(index >= images.length) index = 0;
+      if (index >= images.length) index = 0;
 
       slide.src = images[index].src;
       background.style.backgroundImage = `url(${images[index].src})`;
@@ -46,7 +59,8 @@ function startSlideshow(){
 
       slide.style.opacity = 1;
       caption.style.opacity = 1;
-    }, 1500);
 
-  }, 6000);
+    }, 1000);
+
+  }, 5000);
 }
